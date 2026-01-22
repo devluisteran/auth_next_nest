2cff94a1509fa03f52e1b1efaa13a6aa55b0d676
toy <devluisteran@gmail.com>
2026-01-22 13:22:08 -0600

    implements hashPassword in register

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,11 +1,12 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
+import { PasswordService } from 'src/shared/services/password.service';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly usersService: UsersService){
+    constructor(private readonly usersService: UsersService, private readonly passwordService: PasswordService){
 
     }
     async register(registerDto: RegisterDto){
@@ -14,7 +15,8 @@ export class AuthService {
         if(userFound){
             throw new BadRequestException("El correo ya está en uso");
         }
-        
+        const hashPassword = await this.passwordService.hashPassword(registerDto.password);
+        registerDto.password = hashPassword;
         return await this.usersService.create(registerDto);
     }
 
