67475ac6720634a9b83c0bb0b8b0db9db8fa856c
toy <devluisteran@gmail.com>
2026-01-24 15:36:53 -0600

    update view HomePage

--- frontend/app/dashboard/home/page.tsx
+++ frontend/app/dashboard/home/page.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='font-bold text-4xl mb-4 text-center'>
+            Home Page
+      </h1>
+      <button className='p-3 cursor-pointer rounded-sm bg-blue-600 text-amber-50'>Logout</button>
+    </div>
   )
 }
 
